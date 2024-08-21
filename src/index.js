be390df03c015ff89a7d3ad12ec40e6bd54ead06
anatol-coder <ankawysota@gmail.com>
2024-08-21 06:07:19 +0100

    updated api key

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function searchCity(city) {
-  let apiKey = "5b5b407712f11dct93a10f4f1dc8e2394o";
+  let apiKey = "5b407712f11dct93a10f4f1dc8e2394o";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiKey);
+  console.log(apiUrl);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
