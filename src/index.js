cad4abf7e4b44ff3823ae719cb2313e6a86be148
anatol-coder <ankawysota@gmail.com>
2024-08-21 06:19:38 +0100

    added function in js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+function refreshWeather(response) {
+  console.timeLog(response.data);
+}
+
 function searchCity(city) {
   let apiKey = "5b407712f11dct93a10f4f1dc8e2394o";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 function handleSearchSubmit(event) {
   event.preventDefault();
