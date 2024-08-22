446ca8d317e5e186ec1e1cb4e4aba4748c7c6532
anatol-coder <ankawysota@gmail.com>
2024-08-22 07:42:22 +0100

    Injected more weather data

--- src/index.js
+++ src/index.js
@@ -4,6 +4,35 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
+  let conditionElement = document.querySelector("#condition");
+  conditionElement.innerHTML = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  let windSpeedElement = document.querySelector("#wind-speed");
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
